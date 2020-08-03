0484d8106cc405c3172e81b064b10fb2b604808c
aachalvartak <aachalvartak04@gmail.com>
2020-08-03 21:36:42 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -39,8 +39,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+    	this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -71,13 +71,13 @@ class CommentForm extends Component {
                             <Row className="form-group">
                                 <Label htmlFor="name" md={2}>Your Name</Label>
                                 <Col md={10}>
-                                    <Control.text model=".name" id="name" name="name"
+                                    <Control.text model=".author" id="name" name="name"
                                                   placeholder="Your Name"
                                                   className="form-control"
                                                   validators={{
                                                       required, minLength: minLength(3), maxLength: maxLength(15),
                                                   }}/>
-                                    <Errors className="text-danger" model=".name" show="touched" messages={{
+                                    <Errors className="text-danger" model=".author" show="touched" messages={{
                                         required: 'Required..',
                                         minLength: "Must be greater than 2 characters",
                                         maxLength: "Must be 15 characters or less"
@@ -121,14 +121,14 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
     if (comments != null)
         return (
             <React.Fragment>
                 {comments.map((comment) => {
                     return (
-                        <ul className="list-unstyled" key={comment.id}>
-                            <li>
+                        <ul className="list-unstyled" >
+                            <li key={comment.id} >
                                 <p>{comment.comment}</p>
                                 <p>-- {comment.author} ,{new Intl.DateTimeFormat('en-US', {
                                     year: 'numeric',
@@ -140,12 +140,12 @@ function RenderComments({comments}) {
                     );
                 })}
 
-                <CommentForm/>
+                <CommentForm dishId={dishId} addComment={addComment} />
             </React.Fragment>
         );
     else
         return (
-            <div></div>
+            <div></div> 
         );
 }
 
@@ -174,7 +174,10 @@ const DishDetail = (props) => {
 
                     <div className="col-12 col-lg-5 col-md-5 m-1">
                         <h4>Comments</h4>
-                        <RenderComments comments={props.comments}/>
+                        <RenderComments comments={props.comments}
+					        addComment={props.addComment}
+					        dishId={props.dish.id}
+					    />
 
                     </div>
                 </div>
